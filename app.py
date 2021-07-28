0de98167f9b8eab07c292a1ec5dc80250e57b124
keshavanarayan <74804559+keshavanarayan@users.noreply.github.com>
2021-07-28 10:21:24 +0530

    First test with ML code

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
 from flask import Flask
 import ghhops_server as hs
+import numpy as np
+import string
+import tensorflow as tf
+import tensorflow.keras
+from mymodel import prediction
+import rhino3dm as rg
+
 
 # register hops app as middleware
 app = Flask(__name__)
 hops = hs.Hops(app)
-
 @hops.component(
-    "/test",
-    name="test",
+    "/prediction",
+    name="prediction",
     inputs=[
-        hs.HopsString("Name", "N", "Your name"),
+        hs.HopsNumber("year_to_predict","Z1","Float", hs.HopsParamAccess.LIST),
     ],
     outputs=[
-        hs.HopsString("Welcome", "W", "Welcome to")
+        hs.HopsNumber("metric","Z2","Float", hs.HopsParamAccess.LIST),
+        #hs.HopsNumber("remapped","r","Float" , hs.HopsParamAccess.LIST)
     ]
 )
-def test(n):
-    s = "Welcome, {} to the MaCAD Heroku server".format(n) 
-    return s
+
+def MyMLPred(Z1):
+        output = prediction(Z1)
+        return output
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(debug = True)
